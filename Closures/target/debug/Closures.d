19eed6813fc9b5ebd7dc116bfa43f8bffa0eb36f
Ggtz25 <gabriel.gutierrez06@utrgv.edu>
2026-04-06 18:23:40 +0000

    Closures

---
+++ Closures/target/debug/Closures.d
@@ -0,0 +1,1 @@
+/workspaces/springSystems-01/Closures/target/debug/Closures: /workspaces/springSystems-01/Closures/src/main.rs
---
+++ Closures/target/debug/deps/Closures-23b50cde92e16b6f.d
@@ -0,0 +1,5 @@
+/workspaces/springSystems-01/Closures/target/debug/deps/Closures-23b50cde92e16b6f.d: src/main.rs
+
+/workspaces/springSystems-01/Closures/target/debug/deps/Closures-23b50cde92e16b6f: src/main.rs
+
+src/main.rs:
